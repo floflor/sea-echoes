4cea08da094fa3bdd0d99cb8b913c50b41816775
Flo <inquires.flo@gmail.com>
2023-01-15 21:10:31 +0100

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Loading from "../pages/Loading";
+
+function App() {
+  return (
+    <div className="App">
+      <Loading />
+    </div>
+  );
+}
+
+export default App;
